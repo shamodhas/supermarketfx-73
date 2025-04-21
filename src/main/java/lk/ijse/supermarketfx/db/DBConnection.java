b1dda13c389ada5d5f27263a28aa28501188cad5
shamodhas <shamodha7@gmail.com>
2025-04-21 09:52:11 +0530

    update dependencies

---
+++ src/main/java/lk/ijse/supermarketfx/db/DBConnection.java
@@ -0,0 +1,15 @@
+package lk.ijse.supermarketfx.db;
+
+/**
+ * --------------------------------------------
+ * Author: Shamodha Sahan
+ * GitHub: https://github.com/shamodhas
+ * Website: https://shamodha.com
+ * --------------------------------------------
+ * Created: 4/21/2025 9:14 AM
+ * Project: SupermarketFX
+ * --------------------------------------------
+ **/
+
+public class DBConnection {
+}
